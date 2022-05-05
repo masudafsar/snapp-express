d8273aca03ff408b416e7227191fe8c74898efd7
Masud Afsar <masud@afsar.info>
2022-05-05 18:48:35 +0430

    Config tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: '#ff661f',
+    },
+    fontFamily: {
+      sans: ['Vazirmatn', 'sans-serif'],
+      serif: ['Vazirmatn', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
