f414737f4017a13bf832349fe2ba92d3087b14bc
Masud Afsar <masud@afsar.info>
2022-05-07 01:51:14 +0430

    Implement product list page

---
+++ src/pages/ProductListPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./ProductListPage.module.scss";
+import Page from "../components/page/Page";
+
+const ProductListPage: React.FC = () => {
+  return (
+    <div className={styles.ProductListPage}>
+      <Page>
+
+      </Page>
+    </div>
+  );
+}
+
+export default ProductListPage;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 
+import ProductListPage from "../pages/ProductListPage";
+
 const Router: React.FC = () => {
   return (
     <Routes>
-      <Route path='/' element={<h1>صفحه نخست</h1>} />
-      <Route path='/test' element={<h1>صفحه تست</h1>} />
+      <Route path='/' element={<ProductListPage/>}/>
     </Routes>
   );
 }
