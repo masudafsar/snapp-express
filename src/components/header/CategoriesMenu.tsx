75bf15224a4f39123946b7da9f4233869f77a2e6
Masud Afsar <masud@afsar.info>
2022-05-08 01:07:34 +0430

    Implement category menu styles

--- src/components/header/CategoriesMenu.tsx
+++ src/components/header/CategoriesMenu.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import cn from 'classnames';
+import goToCategoryIcon from "@iconify/icons-ic/twotone-keyboard-arrow-left";
 
 import styles from './CategoriesMenu.module.scss';
 import {Category} from "../../types/category";
+import IconButton from "../elements/button/IconButton";
 
 interface Props {
   show?: boolean;
@@ -12,15 +14,20 @@ interface Props {
 const CategoriesMenu: React.FC<Props> = ({show, categories}) => {
   return (
     <>
-      {categories && (
-        <div className={cn(styles.CategoriesMenu, show ? styles.Show : null)}>
+      {show && categories && (
+        <div className={styles.CategoriesMenu}>
           <span>دسته‌بند‌ی‌ها:</span>
           <div className={styles.Categories}>
             {categories.map(category => {
               return (
                 <div className={styles.Item} key={category.id}>
-                  <img src={category.image} alt={category.title}/>
-                  <span>{category.title}</span>
+                  <img
+                    src={category.image}
+                    alt={category.title}
+                    className={styles.Image}
+                  />
+                  <div className={styles.Title}>{category.title}</div>
+                  <IconButton icon={goToCategoryIcon} color='light' size='large'/>
                 </div>
               );
             })}
