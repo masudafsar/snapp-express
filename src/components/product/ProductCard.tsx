11e12fda824290b9b78ecc09d86534b0b15d76d6
Masud Afsar <masud@afsar.info>
2022-05-08 13:41:01 +0430

    Implement card of product

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import styles from './ProductCard.module.scss';
 import {Product} from "../../types/Product";
 import {digitToPersian} from "../../utils/persian-tools";
+import Button from "../elements/button/Button";
 
 interface Props {
   product: Product;
@@ -11,9 +12,28 @@ interface Props {
 const ProductCard: React.FC<Props> = ({product}) => {
   return (
     <div className={styles.ProductCard}>
-      <div className="border border-2 border-gray-900">
-        {digitToPersian(product.title)}
-        {digitToPersian(`${product.price}`)}
+      <img
+        src={product.featured}
+        alt={digitToPersian(product.title)}
+        className={styles.Image}
+      />
+      <div className={styles.Details}>
+        <div className={styles.Title}>
+          {digitToPersian(product.title)}
+        </div>
+        <div className={styles.Price}>
+          {digitToPersian(`${product.price}`)}
+          {' '}
+          <span>تومان</span>
+        </div>
+        <Button
+          size='normal'
+          variant='outline'
+          color='primary'
+          className={styles.BuyButton}
+        >
+          اضافه به سبد خرید
+        </Button>
       </div>
     </div>
   );
